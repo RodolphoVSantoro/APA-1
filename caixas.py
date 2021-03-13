9cdb44f9961bb62f0674fe90241a3a260d8bc39f
RodolphoVSantoro <44331003+RodolphoVSantoro@users.noreply.github.com>
2021-03-13 15:23:33 -0300

    Add files via upload

---
+++ caixas.py
@@ -0,0 +1,49 @@
+def criaPontos(tmp, linhas, cor):
+	for linha in linhas:
+		xy = linha.split()
+		p = Point(float(xy[0]), float(xy[1]))
+		p.color = cor
+		p.label_visible = False
+		tmp.append(p)	
+
+#inserir caminho completo
+file = open("E:/Rodolpho/Documents/UFF/APA/caixaM1.txt", "r")
+linhas = file.readlines()
+file.close()
+tmp = []
+criaPontos(tmp, linhas, Color.RED)
+$listaM1 =  List(*tmp)
+
+tmp = []
+file = open("E:/Rodolpho/Documents/UFF/APA/caixaM2.txt", "r")
+linhas = file.readlines()
+file.close()
+criaPontos(tmp, linhas, Color.GREEN)
+$listaM2 =  List(*tmp)
+
+tmp = []
+file = open("E:/Rodolpho/Documents/UFF/APA/caixaM3.txt", "r")
+linhas = file.readlines()
+file.close()
+criaPontos(tmp, linhas, Color.BLUE)
+$listaM3 =  List(*tmp)
+
+tmp = []
+file = open("E:/Rodolpho/Documents/UFF/APA/caixaM4.txt", "r")
+linhas = file.readlines()
+file.close()
+criaPontos(tmp, linhas, Color.BLACK)
+$listaM4 =  List(*tmp)
+
+tmp = []
+file = open("E:/Rodolpho/Documents/UFF/APA/caixa.txt", "r")
+linhas = file.readlines()
+file.close()
+criaPontos(tmp, linhas, Color.YELLOW)
+$listaM5 =  List(*tmp)
+
+command("FitLine(listaM1)")
+command("FitLine(listaM2)")
+command("FitLine(listaM3)")
+command("FitLine(listaM4)")
+command("FitLine(listaM5)")
